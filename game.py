6f8be86d11dbe5cc3d5ca0f12025fd993fdc2fa5
nagpalpranay <54006833+nagpalpranay@users.noreply.github.com>
2024-07-21 20:58:08 +0530

    particle effects

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
+import random
 import sys
+import math
 import pygame
 from scripts.entities import PhysicsEntity, Player
 from scripts.utils import load_images, load_img, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
+from scripts.particle import Particle
 
 class Game:
 
@@ -42,6 +45,7 @@ class Game:
             'player/jump': Animation(load_images('entities/player/jump')),
             'player/slide': Animation(load_images('entities/player/slide')),
             'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
+            'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
         }
 
         # print(self.assets)
@@ -49,10 +53,18 @@ class Game:
         self.clouds = Clouds(self.assets['clouds'], count=16)
 
         self.tilemap = Tilemap(self, tile_size=16)
-
+        self.tilemap.load('map.json')
+        
         self.player = Player(self, (50, 50), (8, 15))
 
         self.scroll = [0, 0] # camera's location
+        
+        # spawn leaf particles from trees
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+            
+        self.particles = []
         
     def run(self):
         while True:
@@ -80,6 +92,14 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1])/30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
             
+            for rect in self.leaf_spawners:
+                # random.random() = [0,1) times 49999 here so rate of spawning is lower
+                # and positively correlated to size of rect
+                if random.random() * 49999 < rect.width * rect.height:
+                    # linearly distribute spawning on width and height of rect
+                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+            
             self.clouds.update()
             self.clouds.render(self.display, offset=render_scroll)
             
@@ -87,6 +107,15 @@ class Game:
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display, offset = render_scroll)
+            
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.type == 'leaf':
+                    # add a sway to the particles
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
             
             # print(self.tilemap.tiles_around(self.player.pos))
 
